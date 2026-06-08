c0e67527501233b58d929f52f477fdce053b7033
kagetd511-blip <kagetd511@gmail.com>
2026-06-08 18:55:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+let selectedApp = "";
+
+function toggleSelect(){
+    document.querySelector(".custom-select").classList.toggle("active");
+}
+
+function selectOption(el){
+    let text = el.innerText;
+    let img = el.querySelector("img").src;
+
+    selectedApp = text;
+
+    document.getElementById("selectedText").innerHTML =
+        `<img src="${img}" class="selected-logo"> ${text}`;
+
+    document.querySelector(".custom-select").classList.remove("active");
+}
+
 function kirimWA() {
-    let app = document.getElementById("app").value;
     let nama = document.getElementById("nama").value;
     let hp = document.getElementById("hp").value;
     let jumlah = document.getElementById("jumlah").value;
@@ -7,11 +24,18 @@ function kirimWA() {
 
     let pesan = `Halo, saya ingin pengajuan:
 
-Aplikasi: ${app}
+Aplikasi: ${selectedApp}
 Nama: ${nama}
 No WA: ${hp}
 Total Pinjaman: ${jumlah}
 Jangka Waktu: ${tenor}`;
 
     window.open(`https://wa.me/6281234567890?text=${encodeURIComponent(pesan)}`);
+}
+
+/* TUTUP DROPDOWN */
+window.onclick = function(e){
+    if(!e.target.closest(".custom-select")){
+        document.querySelector(".custom-select").classList.remove("active");
+    }
 }
